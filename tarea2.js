0da054a41871ccb7745515ff5e9c0aa72af00832
unknown <you@example.com>
2019-09-20 09:07:30 -0600

    first commit

---
+++ tarea2.js
@@ -0,0 +1,16 @@
+//Programa para crear un pdf.
+
+let pdf = require('html-pdf');
+
+let contenido = `
+<h1>Esto es un test de html-pdf</h1>
+<p>Estoy generando PDF a partir de este código HTML sencillo</p>
+`;
+
+pdf.create(contenido).toFile('./salida.pdf', function(err, res) {
+    if (err){
+        console.log(err);
+    } else {
+        console.log(res);
+    }
+});
